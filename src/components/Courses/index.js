84512ca13d53bdb50d699bf50f7ddd3d7c0e8f6c
adityacaturputra <adityacaturputra25@gmail.com>
2021-10-21 04:21:35 +0700

    time sorting

--- src/components/Courses/index.js
+++ src/components/Courses/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable radix */
 /* eslint-disable max-len */
 /* eslint-disable react/require-default-props */
 /* eslint-disable react/forbid-prop-types */
@@ -9,11 +10,19 @@ import TitleWithIcon from '../TitleWithIcon';
 import Course from './Course';
 
 export default function Courses({ day, data }) {
+  const timeHHDDToIntFormatter = (time) => {
+    const SplittedTime = time.split(':');
+    return parseInt(SplittedTime[0]);
+  };
+
+  const matkulTimeSorter = (coursesData) => coursesData.sort((course1, course2) => timeHHDDToIntFormatter(course1['start-time']) - timeHHDDToIntFormatter(course2['start-time']));
+
+  const dataSorted = matkulTimeSorter(data);
   return (
     <>
       <TitleWithIcon title={day} Icon={<IconCurrentScheduleTitle size="24px" />} />
       {
-         data.map((courseData) => (
+         dataSorted.map((courseData) => (
            <Course title={courseData.name} place={courseData.place} teacher={courseData.teacher} time={courseData['start-time']} key={courseData.id} />
          ))
       }
