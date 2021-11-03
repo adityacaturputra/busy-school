d36db7a1eca76f66662f63693e1ff0d8cdb83815
adityacaturputra <adityacaturputra25@gmail.com>
2021-11-03 16:46:57 +0700

    profile soon

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Nav from '../../components/Nav';
+import { Wrapper } from '../../style';
 
 function Profile(props) {
   return (
     <>
+      <Wrapper>
+        <h1 style={{ textAlign: 'center' }}>
+          Available Soon
+        </h1>
+      </Wrapper>
       <Nav active="profile" />
     </>
   );
