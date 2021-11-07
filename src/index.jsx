f88a024a9af4f9846c1e951e8544cd273fa64d91
adityacaturputra <adityacaturputra25@gmail.com>
2021-11-07 11:03:02 +0700

    pwa

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import store from './store';
 import theme from './config/theme';
 import App from './App';
 import './index.css';
+
 // import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <BrowserRouter>
@@ -18,7 +20,11 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root'),
 );
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
